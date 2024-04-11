aea1e96a8206c9e5dd00f03302d662343bebd9a4
FerzGT <viktorlfes@rambler.ru>
2024-04-11 19:52:03 +0300

    Even objeck cler

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,18 +1,19 @@
 let clickPage = document.querySelector(".tab__navigation");
-let tabs = document.querySelectorAll(".tab");
-let arrTabs = Array.from(tabs);
-
-let info = document.querySelectorAll(".tab__content");
-let arrInfo = Array.from(info);
+let tabs = [...document.querySelectorAll(".tab")];
+let info = [...document.querySelectorAll(".tab__content")];
 
 clickPage.addEventListener("click", function (e) {
-	let clickElem = event.target;
-	clickElem.classList.toggle("tab_active");
-
-	let i = arrTabs.indexOf(clickElem, 0);
-	console.log(arrInfo);
 	
+	for (let i = 0; i < info.length; i++) {
+		info[i].classList.remove("tab__content_active");
+	}
 
-    arrInfo[i].classList.toggle("tab__content_active");
+	let clickElem = e.target;
+	clickElem.classList.toggle("tab_active");
+	
+	
+	let i = tabs.indexOf(clickElem, 0);
+	console.log(info);
+	info[i].classList.toggle("tab__content_active");
 
 });
