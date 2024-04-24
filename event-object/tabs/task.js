e76ca285928a5db86e411570768f377854d06d2a
FerzGT <viktorlfes@rambler.ru>
2024-04-24 18:46:00 +0300

    Even objeck removeDZ

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,13 +2,11 @@
 let tabs = [...document.querySelectorAll(".tab")];
 let info = [...document.querySelectorAll(".tab__content")];
 
-const removeTabs = tabs.forEach((item) => item.classList.remove("tab_active"));
 
-tabs.forEach((tabs, index1) => {
-	
-	tabs.addEventListener('click', (e) => {
-	
-		removeTabs;
+
+tabs.forEach((tabsc, index1) => {
+	tabsc.addEventListener('click', (e) => {
+		tabs.forEach((item) => item.classList.remove("tab_active"));
 		info.forEach((item) => item.classList.remove("tab__content_active"));
 		e.target.classList.toggle("tab_active");
 		info[index1].classList.toggle("tab__content_active");
