31fa40303e9b1479b28071c4c663d2ea2194d5c0
FerzGT <viktorlfes@rambler.ru>
2024-05-09 08:01:25 +0300

    Todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,38 +1,44 @@
-const taskInput = document.getElementById('task__input')
-const taskList = document.getElementById('tasks__list')
-const tasksAdd = document.getElementById('tasks__add')
+document.addEventListener('DOMContentLoaded', function () {
+  const taskInput = document.getElementById('task__input');
+  const taskList = document.getElementById('tasks__list');
+  const tasksForm = document.getElementById('tasks__form');
 
-function removeElement() {
-  const taskRemove = Array.from(document.querySelectorAll('.task__remove'))
-  taskRemove.forEach((task) => {
-    task.addEventListener('click', (event) => {
-      event.preventDefault()
-      const activeEl = task.closest('.task')
-      activeEl.remove()
-    })
-})
-}
+ 
+  function addTask() {
+    const taskText = taskInput.value.trim();
 
-function inputElement() {
-  tasksAdd.addEventListener('click', (event) => {
-    let text = taskInput.value.trim()
-    if (text.length != 0) {
-      let htmlCode = `
-      <div class="task">
-        <div class="task__title">
-          ${text}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-      </div>
-      `
-      taskList.insertAdjacentHTML('afterEnd', htmlCode)
-      taskInput.value = ''
-    } else {
-        console.log('Строка не может быть пустой!')
+    if (taskText === '') {
+      return;
     }
-    removeElement()
-    event.preventDefault()
-  })
-}
 
-inputElement()
+    const taskElement = document.createElement('div');
+    taskElement.classList.add('task');
+
+    const taskTitle = document.createElement('div');
+    taskTitle.classList.add('task__title');
+    taskTitle.textContent = taskText;
+
+    const removeButton = document.createElement('a');
+    removeButton.href = '#';
+    removeButton.classList.add('task__remove');
+    removeButton.textContent = '×';
+
+   
+    removeButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      taskElement.remove();
+    });
+
+    taskElement.appendChild(taskTitle);
+    taskElement.appendChild(removeButton);
+    taskList.appendChild(taskElement);
+
+    taskInput.value = ''; 
+  }
+
+
+  tasksForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addTask();
+  });
+});
